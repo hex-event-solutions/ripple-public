5435d632cc7319772cc530fee81fb9d9379f37c5
Ollie Nye <ollie@nyemail.co.uk>
2020-06-22 16:02:18 +0100

    404 page

---
+++ src/NotFound.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+const NotFound = () => {
+  return (
+    <div className='container-fluid'>
+      <h1>Oops!</h1>
+      <h3>It doesn't look like we have what you're looking for...</h3>
+    </div>
+  )
+}
+
+export default NotFound
